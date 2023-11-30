d159ea309e68d4dd9e5218d236d54e56ffe831e4
Mihailuch <d1ctators74@yandex.ru>
2023-11-30 20:41:26 +0300

    Параметры документа, окна и работа с ними

--- test/app.js
+++ test/app.js
@@ -1,8 +1,9 @@
 "use strict";
+//! События
 
-let btnList = document.querySelectorAll("button"),
-  wrap = document.querySelector(".wrapper"),
-  link = document.querySelector("a");
+// let btnList = document.querySelectorAll("button"),
+//   wrap = document.querySelector(".wrapper"),
+//   link = document.querySelector("a");
 // btnList[0].onclick = function () {
 //   alert("Привет мир!");
 // };
@@ -31,3 +32,31 @@ let btnList = document.querySelectorAll("button"),
 //     console.log("Событие:: " + e.type + " на элементе " + e.target);
 //   });
 // });
+
+//! Параметры документа
+let box = document.querySelector(".box"),
+  btn = document.querySelector("button");
+
+// let width = box.clientWidth,
+//   height = box.clientHeight;
+
+// let width = box.offsetWidth,
+//   height = box.offsetHeight;
+
+btn.addEventListener("click", function () {
+  //   box.style.height = box.scrollHeight + "px";
+  //   console.log(box.scrollTop);
+  //   box.scrollTop = 0;
+  //   document.documentElement.scrollTop = 0;
+
+  //   document.documentElement.scrollBy(0, 200);
+  document.documentElement.scrollTo(0, 200);
+});
+
+// console.log(width);
+// console.log(height);
+// console.log(box.getBoundingClientRect());
+
+// console.log(document.documentElement.clientWidth);
+// console.log(document.documentElement.clientHeight);
+// console.log(document.documentElement.scrollTop);
