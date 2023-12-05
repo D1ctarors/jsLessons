a514348472734ab4dba7f962f987d9271d75b754
Mihailuch <d1ctators74@yandex.ru>
2023-12-05 12:14:52 +0300

    this

--- test/app.js
+++ test/app.js
@@ -1,6 +1,5 @@
 "use strict";
 //! События
-
 // let btnList = document.querySelectorAll("button"),
 //   wrap = document.querySelector(".wrapper"),
 //   link = document.querySelector("a");
@@ -34,8 +33,8 @@
 // });
 
 //! Параметры документа
-let box = document.querySelector(".box"),
-  btn = document.querySelector("button");
+// let box = document.querySelector(".box"),
+//   btn = document.querySelector("button");
 
 // let width = box.clientWidth,
 //   height = box.clientHeight;
@@ -43,15 +42,15 @@ let box = document.querySelector(".box"),
 // let width = box.offsetWidth,
 //   height = box.offsetHeight;
 
-btn.addEventListener("click", function () {
-  //   box.style.height = box.scrollHeight + "px";
-  //   console.log(box.scrollTop);
-  //   box.scrollTop = 0;
-  //   document.documentElement.scrollTop = 0;
+// btn.addEventListener("click", function () {
+//   //   box.style.height = box.scrollHeight + "px";
+//   //   console.log(box.scrollTop);
+//   //   box.scrollTop = 0;
+//   //   document.documentElement.scrollTop = 0;
 
-  //   document.documentElement.scrollBy(0, 200);
-  document.documentElement.scrollTo(0, 200);
-});
+//   //   document.documentElement.scrollBy(0, 200);
+//   document.documentElement.scrollTo(0, 200);
+// });
 
 // console.log(width);
 // console.log(height);
@@ -60,3 +59,81 @@ btn.addEventListener("click", function () {
 // console.log(document.documentElement.clientWidth);
 // console.log(document.documentElement.clientHeight);
 // console.log(document.documentElement.scrollTop);
+
+//! Конструкторы и классы
+// function User(name, id) {
+//   this.name = name;
+//   this.id = id;
+//   this.human = true;
+//   this.hello = function () {
+//     console.log("Привет! " + this.name);
+//   };
+// }
+
+// User.prototype.exit = function (name) {
+//   console.log("Пользователь " + this.name + " ушёл");
+// };
+
+// let Ivan = new User("Ivan", 25);
+
+// let Alex = new User("Alex", 20);
+
+// console.log(Ivan.exit());
+// console.log(Alex);
+
+//! This
+//? 1) Просто вызов функции
+// function showThis(a, b) {
+//   console.log(this);
+//   function sum() {
+//     console.log(this);
+//     return this.a + this.b;
+//   }
+//   console.log(sum());
+// }
+// showThis(4, 5);
+// showThis(5, 5);
+
+//? 2) Метод объекта this = объект
+// let obj = {
+//   a: 20,
+//   b: 15,
+//   sum: function () {
+//     console.log(this);
+//   },
+// };
+// obj.sum();
+
+//? 3) Конструктор (new) - this = новый созданный объект
+
+//? 4) Жесткая привязка контекста - call, applay, bind
+// let user = {
+//   name: "John",
+// };
+
+// function sayName(surname) {
+//   console.log(this);
+//   console.log(this.name + surname);
+// }
+
+// console.log(sayName.call(user, " Smith"));
+// console.log(sayName.apply(user, [" Show"]));
+
+// function count(number) {
+//   return this * number;
+// }
+
+// let double = count.bind(2);
+// console.log(double(3));
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function () {
+//   console.log(this);
+//   this.style.backgroundColor = "red";
+
+//   function showThis() {
+//     console.log(this);
+//   }
+//   showThis();
+// });
